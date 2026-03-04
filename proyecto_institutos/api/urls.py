6aa35751244472db71b571825270946c2de430b3
Carolina-Vazquez <caroldesarrolloweb00@gmail.com>
2026-03-04 12:14:41 +0100

    Update urls.py

--- proyecto_institutos/api/urls.py
+++ proyecto_institutos/api/urls.py
@@ -4,4 +4,5 @@ from .views import InstitutoListView, InstitutoDetailView
 urlpatterns = [
     path('institutos/',          InstitutoListView.as_view(),   name='instituto-list'),
     path('institutos/<int:pk>/', InstitutoDetailView.as_view(), name='instituto-detail'),
+    
 ]
